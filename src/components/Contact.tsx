bbf16c8fb068b995645b3884aa1db1277a80c675
瀬島大生 <sejimadaisei@sejimadaiseinoMacBook-Pro.local>
2022-10-24 17:16:27 +0900

    Created Contact Components

---
+++ src/components/Contact.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Box from "@mui/system/Box"
+import Typography from '@mui/material/Typography'
+import { Link } from '@mui/material'
+import Icon from '@mui/material'
+import Avatar from "@mui/material/Avatar"
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+
+const Contact = () => {
+    return(
+        <></>
+    );
+}
+export default Contact;
