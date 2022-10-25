343c520b7f2210777b6ea0b294063f0273b98cd7
瀬島大生 <sejimadaisei@sejimadaiseinoMacBook-Pro.local>
2022-10-25 14:14:08 +0900

    add contact button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import MyAvatar from './components/MyAvatar';
 import About from './components/About';
 import Skills from './components/Skills'
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <MyAvatar />
     <About />
     <Skills />
+    <Contact />
     </>
   );
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,10 +7,37 @@ import Avatar from "@mui/material/Avatar"
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
+import { blue, green, grey, purple, red } from '@mui/material/colors'
 
 const Contact = () => {
     return(
-        <></>
+        <>
+        <Box p={2}>
+            <Box display='flex' justifyContent='center'>
+                <Typography variant='h5'>
+                    Contact
+                </Typography>
+            </Box>
+            <Box display='flex' p={2} justifyContent='center'>
+            <Link href='#' color='inherit' sx={{marginLeft:1}}>
+            <Avatar>
+              <EmailIcon />
+            </Avatar>
+          </Link>
+          <Link href='#' color='inherit' sx={{marginLeft:1}}>
+            <Avatar sx={{bgcolor:grey[900]}}>
+              <GitHubIcon />
+            </Avatar>
+          </Link>
+          <Link href='#' color='inherit' sx={{marginLeft:1}}>
+            <Avatar sx={{bgcolor: blue[500]}}>
+              <TwitterIcon />
+            </Avatar>
+          </Link>
+            </Box>
+
+        </Box>
+        </>
     );
 }
 export default Contact;
